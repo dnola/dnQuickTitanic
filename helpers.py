deafa0830c154d5fc4681159e0137111eebfae5c
davidnola <d.nola@me.com>
2015-12-01 14:42:17 -0800

    more info

--- helpers.py
+++ helpers.py
@@ -4,8 +4,8 @@ import pandas as pd
 class PandasEncoder:                # All we need to put something in a pipeline is a class with fit(self,X,y=None) and transform(self,X)
     def fit(self,X,y=None):
         X = pd.get_dummies(X) # literally means 'one hot encode' - its a new pandas function
-        self.columns = X.columns
+        self.columns_in_test_data = X.columns
         return self                 # fit() returns self
 
     def transform(self,X): # transform() returns the transformed data
-        return pd.get_dummies(X).reindex(columns = self.columns, fill_value=0) # Sometimes test data and train data have different values in them - so we throw away anything that we havent seen in train data
+        return pd.get_dummies(X).reindex(columns = self.columns_in_test_data, fill_value=0) # Sometimes test data and train data have different columns in them after one hot encoding - so we throw away anything that we havent seen in train data
